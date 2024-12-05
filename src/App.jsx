5e9476afa26ef653d1d1a3007b4328ef233ea7c8
Angelito <litsdionisio@gmail.com>
2024-12-05 19:30:12 +0800

    Setting up react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div className=""></div>;
+  return <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </>;
 };
 
 export default App;
