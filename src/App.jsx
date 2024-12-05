6f47d89c8813361d5c4365b1fd4d3ab663220def
Angelito <litsdionisio@gmail.com>
2024-12-05 19:45:08 +0800

    Created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<Signup />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
